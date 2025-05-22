fdbbed53ff25aa1f606036e3b249184d1c3e2c11
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-05-22 12:04:20 +0530

    added zod for structured output parser

--- langchainJS_tutorial/3_output-parsers.js
+++ langchainJS_tutorial/3_output-parsers.js
@@ -5,27 +5,24 @@ import {
   StringOutputParser,
   CommaSeparatedListOutputParser,
 } from "@langchain/core/output_parsers";
+import { StructuredOutputParser } from "langchain/output_parsers";
 import logger from "./logger.js";
+import { z } from "zod";
 
 dotenv.config();
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.0-flash",
   //   verbose: true,
-  maxOutputTokens: 30,
+  maxOutputTokens: 50,
 });
 
+// ----------------------  for string ----------------------------------
 const prompt_template_string_output_parser = ChatPromptTemplate.fromMessages([
   ["system", "Generate a joke based on  a word provided by the user"],
   ["human", "{input}"],
 ]);
-
-const prompt_template_list_output_parser = ChatPromptTemplate.fromTemplate(`
-    provide 5 synonyms, separated by commas, for the following Word {word}
-    `);
-
 const string_parser = new StringOutputParser();
-const comma_separated_parser = new CommaSeparatedListOutputParser();
 
 async function callStringOutputParser() {
   try {
@@ -40,6 +37,13 @@ async function callStringOutputParser() {
   }
 }
 
+// ----------------------  for comma separated list ----------------------------------
+const prompt_template_list_output_parser = ChatPromptTemplate.fromTemplate(`
+    provide 5 synonyms, separated by commas, for the following Word {word}
+    `);
+
+const comma_separated_parser = new CommaSeparatedListOutputParser();
+
 async function callCommaSeparatedListOutputParser() {
   let chain = prompt_template_list_output_parser
     .pipe(model)
@@ -49,5 +53,60 @@ async function callCommaSeparatedListOutputParser() {
   logger.info("using pipeline comma-separated-output-parser", response);
 }
 
+// ----------------------  for structured output parser ----------------------------------
+const prompt_template_structured_parser = ChatPromptTemplate.fromTemplate(`
+    Extract information from the following phrase.
+    Formatting Instructions :{format_instructions}.
+    Phrase: {phrase}
+    `);
+const structured_parser = StructuredOutputParser.fromNamesAndDescriptions({
+  name: "the name of the person",
+  age: "the age of the person",
+});
+
+async function callStructureOutputParser() {
+  const chain = prompt_template_structured_parser
+    .pipe(model)
+    .pipe(structured_parser);
+  const response = await chain.invoke({
+    phrase: "Max in 30 years",
+    format_instructions: structured_parser.getFormatInstructions(),
+  });
+  console.log(response);
+  logger.info("structured output", response);
+  console.log(typeof response);
+}
+
+// ----------------------  for structured zod schema - output parser ----------------------------------
+const prompt_template_structured_zod_parser = ChatPromptTemplate.fromTemplate(`
+    Extract information from the following phrase.
+    Formatting Instructions :{format_instructions}.
+    Phrase: {phrase}
+    `);
+const structured_zod_parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    recipe: z.string().describe("name of recipe"),
+    ingredients: z.array(z.string()).describe("ingredients"),
+  })
+);
+
+async function callStructureZodOutputParser() {
+  const chain = prompt_template_structured_zod_parser
+    .pipe(model)
+    .pipe(structured_zod_parser);
+  const response = await chain.invoke({
+    phrase:
+      "The ingredients for a Spaghetti Bolognese recipe are tomato, minced beef, garlic, wine and herbs.",
+    format_instructions: structured_zod_parser.getFormatInstructions(),
+  });
+  console.log(response);
+  logger.info("structured zod output", response);
+  console.log(typeof response);
+}
+
 // callStringOutputParser();
-callCommaSeparatedListOutputParser();
+// callCommaSeparatedListOutputParser();
+// callStructureOutputParser();
+// console.log(structured_zod_parser.getFormatInstructions());
+
+callStructureZodOutputParser();
