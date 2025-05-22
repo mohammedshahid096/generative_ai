d1f87293b40bcc6bcc4bedba41fd0bbcfafd4a17
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-05-22 10:58:41 +0530

    prompt_template is added

---
+++ langchainJS_tutorial/prompt_template.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+dotenv.config();
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.0-flash",
+  //   verbose: true,
+  maxOutputTokens: 30,
+});
+
+const prompt_template_name = ChatPromptTemplate.fromTemplate(
+  "Tell me a {adjective} joke about {topic}."
+);
+
+const prompt_template_messages = ChatPromptTemplate.fromMessages([
+  ["system", "Generate a joke based on  a word provided by the user"],
+  ["human", "{input}"],
+]);
+
+async function usingPromptFormat() {
+  let prompt = await prompt_template_name.format({
+    adjective: "funny",
+    topic: "robots",
+  });
+  const response = await model.invoke(prompt);
+  console.log(response);
+}
+
+async function usingPipe() {
+  let chain = prompt_template_name.pipe(model);
+  const response = await chain.invoke({ adjective: "funny", topic: "robots" });
+  console.log(response);
+}
+// usingPromptFormat();
+// usingPipe();
